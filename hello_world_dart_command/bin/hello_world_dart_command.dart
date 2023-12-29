018599e465f928611a778781cf16b60125af0f39
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2023-12-29 00:41:28 -0600

    [Agregnado][Configuracion]: Agregando configuracion de vs code para ejecutar diferentes main

--- hello_world_dart_command/bin/hello_world_dart_command.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hello_world_dart_command/hello_world_dart_command.dart' as hello_world_dart_command;
-
-void main(List<String> arguments) {
-  print('Hello world: ${hello_world_dart_command.calculate()}!');
-  print(arguments);
-}
