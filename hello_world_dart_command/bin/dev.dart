6e8af92b5798e37f98a7df43809c9727bd2f3985
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2023-12-29 13:53:07 -0600

    [Agregando][TiposDatos]: Ejemplos con constantes

--- hello_world_dart_command/bin/dev.dart
+++ hello_world_dart_command/bin/dev.dart
@@ -13,51 +13,21 @@ void main(List<String> arguments) {
         # dynamic
 
       - Inmutables
-        # finaly: asignados en tipo de ejecucion
-        # contat: asignados antes de compilar
+        # final: asignados en tipo de ejecucion
+        # const: asignados antes de compilar
 
   */
-  int ageOfPet = 10;
-  double priceOfPet = 100.99;
-  bool enable = false;
-  String namePet = 'Meteoro';
-
-  var lastNamePet = 'Sun';
-  var visitPet = 2.0;
-
-  dynamic valorDinamico = "Eber :v";
-  print(valorDinamico.runtimeType);
-  valorDinamico = 1;
-  valorDinamico = false;
-  print(valorDinamico.runtimeType);
-
-  Object obj;
-
-  final opTxt = OperacionesTexto();
-  print(opTxt.concatenacion("hola", "mundo"));
-  print(opTxt.concatenacionNumero("hola", 27));
-  print(opTxt.concatenacionDinamico("hola", false));
-  print(opTxt.longitudCadena("hola mundo"));
+  final String apiId = ""; // Valor que se obtiene en tipo de ejecucion
+  const String apiKey = "My_Api_Key"; // Valor que ya sabemos de antemano
+  const lulu = Pet();
+  final fresita = Car();
 }
 
-class OperacionesTexto {
-  String concatenacion(String txt1, String txt2) {
-    return txt1 + txt2;
-  }
+class Pet {
+  //TODO: Constructor, de tipo const para poder ser un tipo de dato inmutable
+  const Pet();
+}
 
-  String concatenacionNumero(String txt1, int num) {
-    return txt1 + num.toString();
-  }
-
-  String concatenacionDinamico(String txt, dynamic value) {
-    return 'Texto: $txt - Dinamico: $value';
-  }
-
-  String longitudCadena(String cadena) {
-    return '${cadena.length}';
-  }
-
-  String getFechaActual() {
-    return '${DateTime.now()}';
-  }
+class Car {
+  Car();
 }
