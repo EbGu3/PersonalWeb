a25cc79a73ff609f390ddfc436e89d79f3037794
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2023-12-29 00:56:15 -0600

    [Agregando][Main]: comentarios, metodos y clases xD, tengo sueño z:o

--- hello_world_dart_command/bin/dev.dart
+++ hello_world_dart_command/bin/dev.dart
@@ -1,3 +1,27 @@
 void main() {
   print("dev mode");
+  print(suma(2, 2));
+
+  final calculadora = Calculadora();
+  print(calculadora.suma(2, 3));
+}
+
+/// Clase, que realiza operaciones matematicas :V
+class Calculadora {
+  ///Suma de dos numeros enteros
+  ///[a] Valor entero numero 1
+  ///[b] Valor entero numero 2
+  ///```
+  ///suma(1, 4) = 5;
+  ///```
+  int suma(int a, int b) {
+    //  Comentario de linea :v
+    /* 
+        Comentario 
+        de 
+        bloque 
+        v:
+    */
+    return a + b;
+  }
 }
