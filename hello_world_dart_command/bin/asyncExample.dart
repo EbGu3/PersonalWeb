a081a06c1bb50acb7ec34bd1a6d1a056b9ba0660
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-01-17 21:35:55 -0600

    [Ejemplo][Future]: Ejemplos de async, await, Future

--- hello_world_dart_command/bin/asyncExample.dart
+++ hello_world_dart_command/bin/asyncExample.dart
@@ -1,13 +1,40 @@
-void main(List<String> arguments) {
+import 'dart:async';
+
+void main(List<String> arguments) async {
   print("hola");
-  final f = sleep(2);
-  f.then((value) {
-    print(value);
-  });
+  // final f = sleepError(2);
+  // f.then((value) {
+  //   print(value);
+  // }).onError((error, stackTrace) {
+  //   print("Error Mensaje: $error, StackTrace: $stackTrace");
+  // });
+
+  //  TODO: Usando async y await
+  final respuesta = await sleep(2);
+  print(respuesta);
+  await sleepVoid(2);
+
+  print("end");
 }
 
+//   TODO: Retornar un tipo de dato string
 Future<String> sleep(int segundos) {
   return Future.delayed(Duration(seconds: segundos), () {
     return "❌";
+  });
+}
+
+//  TODO: Retornar un error
+Future<String> sleepError(int segundos) {
+  return Future.delayed(Duration(seconds: segundos), () {
+    throw RangeError("Error de ejemplo");
+    return "❌";
+  });
+}
+
+//  TODO: Retornar cualquier tipo de dato
+Future<void> sleepVoid(int segundos) {
+  return Future.delayed(Duration(seconds: segundos), () {
+    print("Ivanna 🥰");
   });
 }
